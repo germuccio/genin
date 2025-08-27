16a96e9be170396b3baa742354dee5597061ef91
germuccio <germantas.dikcius@gmail.com>
2025-08-27 17:20:04 +0300

    fix: Inline dependencies in disconnect endpoint for Vercel

--- api/auth/visma/disconnect.js
+++ api/auth/visma/disconnect.js
@@ -1,15 +1,16 @@
-const { setCors } = require('../../_utils');
-const statusModule = require('../../_status');
+// Inlined setCors for Vercel compatibility
+function setCors(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+}
 
 module.exports = async (req, res) => {
   setCors(res);
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  // Check authentication
   if (req.method === 'DELETE') {
-    // Clear the in-memory store
-    statusModule.setTokens(null);
-    
     // Clear the HttpOnly cookie by setting its expiration date to the past
     res.setHeader('Set-Cookie', 'visma-tokens=; HttpOnly; Path=/; Secure; SameSite=Strict; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
     
